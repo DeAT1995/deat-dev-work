9837da0e7ae98a88198c7e9cc8d2fcac475f9949
Debora Tavares <debora2013aguiartavares@gmail.com>
2025-01-30 00:05:34 -0300

    feat: created the pages about, contact and curriculum

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 
 export const routes: Routes = [{ path: '', component: HomeComponent }];
